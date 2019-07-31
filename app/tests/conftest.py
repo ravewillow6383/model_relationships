87b87d9e7a86a3a8ac17af6d2bdfb7d92790ce76
Raven Robertson <ravewillow6383@gmail.com>
2019-07-30 18:38:44 -0700

    fixtures built

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,9 +3,55 @@ from app import create_app, db
 from config import Config
 from app.models import Creature, Winged_Creature
 
-class testConfig(Config):
+class TestConfig(Config):
 
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+
+@pytest.fixture()
+def client():
+
+    app = create_app(TestConfig)
+
+    app_context = app.app_context()
+
+    app_context.push()
+
+    db.create_all()
+
+    with app.test_client() as client:
+        yield client
+
+    db.session.remove()
+
+    db.drop_all()
+
+    app_context.pop()
+
+
+@pytest.fixture()
+def sample_creature(client):
+    creature = Creature(name='Birds')
+    db.session.add(Creature)
+    db.session.commit()
+    return creature
+
+@pytest.fixture()
+def sample_winged_creature():
+    winged_creature = Winged_Creature(name='Flightless cormorant', creature=sample_creature)
+    db.session.add(winged_creature)
+    db.commit()
+    return winged_creature
+
+@pytest.fixture()
+def lone_winged_creature():
+    winged_creature = Winged_Creature(name='Zburator')
+    db.session.add(winged_creature)
+    db.commit()
+    return winged_creature
+
+
+
 
 
 
